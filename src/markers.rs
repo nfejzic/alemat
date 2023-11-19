70477d81ac208b946ff72b40c426fb9f8c65bccd
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-19 16:00:50 +0100

    feat: derive useful traits for marker structs

--- src/markers.rs
+++ src/markers.rs
@@ -1,2 +1,5 @@
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Init;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Uninit;
