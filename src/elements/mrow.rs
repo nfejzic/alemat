6cb213e1fa015744c556065458426b7b66e6c4c8
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-21 22:25:28 +0100

    feat: improve API of `mrow`

--- src/elements/mrow.rs
+++ src/elements/mrow.rs
@@ -20,3 +20,14 @@ where
         }
     }
 }
+
+impl Row {
+    pub fn add_attr<I, A>(mut self, attr: I) -> Self
+    where
+        I: IntoIterator<Item = A>,
+        A: Into<Attribute>,
+    {
+        self.attr.extend(attr.into_iter().map(Into::into));
+        self
+    }
+}
