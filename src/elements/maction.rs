6da8df0febbdebe9103a9dbf87e2923e251d2b12
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-21 11:35:59 +0100

    feat: improve docs and impl builder for `maction`

--- src/elements/maction.rs
+++ src/elements/maction.rs
@@ -1,16 +1,45 @@
-use crate::{attributes::Attribute, MathMl};
+use std::marker::PhantomData;
 
-/// An attribute of `maction` element. Either one of the global [`Attribute`]s, or `encode`
-/// attribute.
+use crate::{
+    attributes::Attribute,
+    markers::{Init, Uninit},
+    MathMl,
+};
+
+/// The `maction` element accepts the global [`Attribute`]s as well as `selection` and
+/// `actiontype`.
 ///
 /// [`Attribute`]: crate::attributes::Attribute
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ActionAttr {
     Global(Attribute),
+    /// The child element currently visible, only taken into account for `actiontype="toggle"` or
+    /// non-standard actiontype values. The default value is `1`, which is the first child element.
     Selection(String),
+
+    /// The action which specifies what happens for this element. Special behavior for the
+    /// following values were implemented by some browsers:
+    ///
+    /// * `statusline`: If there is a click on the expression or the reader moves the pointer over
+    ///   it, the message is sent to the browser's status line. The syntax is:
+    ///
+    ///   ```html
+    ///   <maction actiontype="statusline"> expression message </maction>.
+    ///   ```
+    ///
+    /// * `toggle`: When there is a click on the subexpression, the rendering alternates the
+    ///    display of selected subexpressions. Therefore each click increments the selection value.
+    ///    The syntax is:
+    ///
+    ///   ```html
+    ///   <maction actiontype="toggle" selection="positive-integer">
+    ///       expression1 expression2 expressionN
+    ///   </maction>.
+    ///   ```
+    ActionType(String),
 }
 
-/// The `annotation` (and `annotation-xml`) element.
+/// Historically, the `maction` element provides a mechanism for binding actions to expressions.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Action {
     content: MathMl,
@@ -25,6 +54,15 @@ impl Action {
             attributes: Default::default(),
         }
     }
+
+    pub fn builder() -> ActionBuilder<Uninit> {
+        ActionBuilder {
+            content: None,
+            attributes: Default::default(),
+
+            _marker: PhantomData,
+        }
+    }
 }
 
 impl<T> From<T> for Action
@@ -35,6 +73,43 @@ where
         Self {
             content: value.into(),
             attributes: Default::default(),
+        }
+    }
+}
+
+pub struct ActionBuilder<T> {
+    content: Option<MathMl>,
+    attributes: Vec<ActionAttr>,
+
+    _marker: PhantomData<(T,)>,
+}
+
+impl<T> ActionBuilder<T> {
+    pub fn content(self, content: impl Into<MathMl>) -> ActionBuilder<Init> {
+        ActionBuilder {
+            content: Some(content.into()),
+            attributes: self.attributes,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn attr<I, A>(mut self, attributes: I) -> Self
+    where
+        I: IntoIterator<Item = A>,
+        A: Into<ActionAttr>,
+    {
+        self.attributes
+            .extend(attributes.into_iter().map(Into::into));
+
+        self
+    }
+}
+
+impl ActionBuilder<Init> {
+    pub fn build(self) -> Action {
+        Action {
+            content: self.content.expect("Content is guaranteed to be init."),
+            attributes: self.attributes,
         }
     }
 }
