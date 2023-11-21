81d3baad4dca448027d1f97d41b4c9e992ec005c
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-21 22:20:24 +0100

    feat: implement building of `phantom`

--- src/elements/mphantom.rs
+++ src/elements/mphantom.rs
@@ -28,3 +28,14 @@ where
         }
     }
 }
+
+impl Phantom {
+    pub fn add_attr<I, A>(mut self, attr: I) -> Self
+    where
+        I: IntoIterator<Item = A>,
+        A: Into<Attribute>,
+    {
+        self.attributes.extend(attr.into_iter().map(Into::into));
+        self
+    }
+}
