c89aa732775a2251b325caadb6536429679f5cad
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-21 22:25:04 +0100

    feat: implement builder for radicals

--- src/elements/mroot.rs
+++ src/elements/mroot.rs
@@ -1,4 +1,10 @@
-use crate::{attributes::Attribute, MathMl};
+use std::marker::PhantomData;
+
+use crate::{
+    attributes::Attribute,
+    markers::{Init, Uninit},
+    MathMl,
+};
 
 /// The radical elements construct an expression with a root symbol `√` with a line over the content.
 /// The msqrt element is used for square roots, while the mroot element is used to draw radicals
@@ -11,4 +17,58 @@ pub struct Radicals {
     index: String,
     content: MathMl,
     attributes: Vec<Attribute>,
+}
+
+impl Radicals {
+    pub fn builder() -> RadicalsBuilder<Uninit, Uninit> {
+        RadicalsBuilder::default()
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RadicalsBuilder<T1, T2> {
+    index: Option<String>,
+    content: Option<MathMl>,
+    attr: Vec<Attribute>,
+
+    _marker: PhantomData<(T1, T2)>,
+}
+
+impl<T1, T2> RadicalsBuilder<T1, T2> {
+    pub fn index(self, index: impl Into<String>) -> RadicalsBuilder<Init, T2> {
+        RadicalsBuilder {
+            index: Some(index.into()),
+            content: self.content,
+            attr: self.attr,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn content(self, content: impl Into<MathMl>) -> RadicalsBuilder<T1, Init> {
+        RadicalsBuilder {
+            index: self.index,
+            content: Some(content.into()),
+            attr: self.attr,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn attr<I, A>(mut self, attr: I) -> Self
+    where
+        I: IntoIterator<Item = A>,
+        A: Into<Attribute>,
+    {
+        self.attr.extend(attr.into_iter().map(Into::into));
+        self
+    }
+}
+
+impl RadicalsBuilder<Init, Init> {
+    pub fn build(self) -> Radicals {
+        Radicals {
+            index: self.index.expect("Index is guaranteed to be init."),
+            content: self.content.expect("Content is guaranteed to be init."),
+            attributes: self.attr,
+        }
+    }
 }
