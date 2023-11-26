605b0ad3d5b4e012dd15d52798ec6b13f28d17b5
Nadir Fejzic <nadirfejzo@gmail.com>
2023-11-26 13:56:48 +0100

    feat: prepare rendering of `MathMl`

--- src/lib.rs
+++ src/lib.rs
@@ -38,10 +38,49 @@ pub(crate) enum Tag {
     UnderOver(UnderOver),
 }
 
+impl ToMathMl for Tag {
+    fn to_mathml(&self) -> String {
+        match self {
+            Self::Action(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Annotation(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Error(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Frac(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Ident(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Multiscripts(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Num(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Operator(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Padded(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Phantom(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Radical(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Row(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Semantics(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Space(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::StrLiteral(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Style(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::SubSup(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Table(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::Text(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+            Self::UnderOver(el) => unimplemented!("ToMathMl not implemented for {el:?}"),
+        }
+    }
+}
+
 #[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct MathMl {
     content: Vec<Tag>,
     // TODO: decide what fields should go inside
+}
+
+impl ToMathMl for MathMl {
+    fn to_mathml(&self) -> String {
+        let content = self
+            .content
+            .iter()
+            .map(ToMathMl::to_mathml)
+            .collect::<String>();
+
+        format!("<math>{content}</math>")
+    }
 }
 
 macro_rules! from_types {
