07757efa9c6afb77c281c104dfb71bf88a93fe71
Nadir Fejzic <nadirfejzo@gmail.com>
2023-12-06 23:05:02 +0100

    docs: document marker types

--- src/markers.rs
+++ src/markers.rs
@@ -1,7 +1,13 @@
+//! Marker types used in builders.
+
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[doc(hidden)]
+/// Marker type used in builders to indicate that a particular value has been initialized.
+/// This is not intended to be used dirctly.
 pub struct Init;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[doc(hidden)]
+/// Marker type used in builders to indicate that a particular value has not been initialized.
+/// This is not intended to be used dirctly.
 pub struct Uninit;
